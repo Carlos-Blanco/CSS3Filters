2760388512211dabf09674beda52c0da92373408
Carlos Blanco <carlosblanco@me.com>
2012-12-13 17:53:01 +0100

    CSS3 Filters
    CSS· image filters in Webkit browsers

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function saturate(){
+	$("#image").removeClass();
+	$("#image").addClass("saturate");
+}
+function grayscale(){
+	$("#image").removeClass();
+	$("#image").addClass("grayscale");
+}
+function contrast(){
+	$("#image").removeClass();
+	$("#image").addClass("contrast");
+}
+function brightness(){
+	$("#image").removeClass();
+	$("#image").addClass("brightness");
+}
+function blure(){
+	$("#image").removeClass();
+	$("#image").addClass("blur");
+}
+function invert(){
+	$("#image").removeClass();
+	$("#image").addClass("invert");
+}
+function sepia(){
+	$("#image").removeClass();
+	$("#image").addClass("sepia");
+}
+function huerotate(){
+	$("#image").removeClass();
+	$("#image").addClass("huerotate");
+}
+function opacity(){
+	$("#image").removeClass();
+	$("#image").addClass("opacity");
+}
